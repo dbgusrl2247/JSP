310a36394cb3231baa19fc297781eefdb507079a
dbgusrl2247 <yhk041018@gmail.com>
2026-04-12 23:01:09 +0900

    불필요파일 제거

--- JSP-main/JSPBook/src/main/java/javax/servlet/http/HttpServlet.java
+++
@@ -1,6 +0,0 @@
-package javax.servlet.http;
-
-public class HttpServlet {
-    // Minimal stub to satisfy JSP compilation in the IDE.
-    // Do NOT use this in production; the real servlet API should be provided by the server runtime (Tomcat).
-}
